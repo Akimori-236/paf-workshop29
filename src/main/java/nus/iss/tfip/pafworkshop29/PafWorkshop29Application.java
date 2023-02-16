e7818a3c58519ab3dd0258dd9b35e12b6de547ef
Wee Seng <wee.bscl@gmail.com>
2023-02-16 11:41:51 +0800

    springboot init

---
+++ src/main/java/nus/iss/tfip/pafworkshop29/PafWorkshop29Application.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.pafworkshop29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafWorkshop29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafWorkshop29Application.class, args);
+	}
+
+}
