e5a8371a40f320277049b4d1081cc39433bf0734
Wee Seng <wee.bscl@gmail.com>
2023-02-28 23:51:26 +0800

    EOD valid vs validated test

--- src/main/java/nus/iss/tfip/pafworkshop29/controller/OrderController.java
+++ src/main/java/nus/iss/tfip/pafworkshop29/controller/OrderController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ public class OrderController {
         @Autowired
         private OrderService orderSvc;
 
+        @Validated
         @PostMapping(path = { "/order",
                         "/order/" }, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
         public ResponseEntity<String> postOrderForm(@RequestBody Order order) {
--- src/main/java/nus/iss/tfip/pafworkshop29/model/Order.java
+++ src/main/java/nus/iss/tfip/pafworkshop29/model/Order.java
@@ -3,12 +3,14 @@ package nus.iss.tfip.pafworkshop29.model;
 import java.sql.Date;
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import nus.iss.tfip.pafworkshop29.Constants;
 
 @Data
 public class Order implements Constants {
     private String orderId;
+    @NotNull(message = "please enter name")
     private String name;
     private String email;
     private Date deliveryDate;
